2983c179d291a9e884e715ef16f73e2364bec1f4
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-11-01 10:35:12 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+
+  def initialize sideOne, sideTwo, sideThree
+    @sideOne = sideOne
+    @sideTwo = sideTwo
+    @sideThree = sideThree
+  end
+  def kind
+    triangle_sides = [@sideOne, @sideTwo, @sideThree].sort
+    if triangle_sides.any?{|side| side <= 0} || (triangle_sides[0] + triangle_sides[1] <= triangle_sides[2])
+      raise TriangleError
+    elsif @sideOne == @sideTwo && @sideOne == @sideThree
+      :equilateral
+    elsif @sideOne == @sideTwo || @sideOne == @sideThree || @sideTwo == @sideThree
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
